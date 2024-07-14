354943096b20a3695fd003b4f5a326bcd8800a5d
Artem Kozhevnikov <artekof.one@gmail.com>
2024-07-14 13:10:16 +0300

    Готово 1ое задание

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework1/ApplicationHomework1.java
@@ -0,0 +1,14 @@
+package ru.otus.java.basic.homeworks.homework1;
+
+public class ApplicationHomework1 {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+
+//    (1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова:
+//    Hello, World, from, Java;
+    public static void greetings(){
+        System.out.println("Hello\nWorld\nfrom\nJava");
+    }
+}
