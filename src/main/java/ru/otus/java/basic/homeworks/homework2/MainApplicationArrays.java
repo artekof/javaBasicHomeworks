7b33245db215f6faf72123f9b2385f3ec1186ccc
Artem <artekof.one@gmail.com>
2024-07-21 14:15:28 +0300

    Третье задание готово.

--- src/main/java/ru/otus/java/basic/homeworks/homework2/MainApplicationArrays.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework2/MainApplicationArrays.java
@@ -1,11 +1,13 @@
 package ru.otus.java.basic.homeworks.homework2;
 
+import java.util.Arrays;
+
 public class MainApplicationArrays {
     public static void main(String[] args) {
         int[] arr = {1, 5, 8, 10, 7, 2, 10};
         //counterStringsOutput(10,"Проверка");
-        sumArgsArray(arr);
-
+        //sumArgsArray(arr);
+        //fillArrays(5, arr);
     }
 
 //    1. Реализуйте метод, принимающий в качестве аргументов целое число и строку,
@@ -28,7 +30,17 @@ public class MainApplicationArrays {
             }
         }
         System.out.println("Сумма элементов массива, значение которых больше 5 = " + sumElement);
-
     }
+
+//    3. Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на целочисленный массив,
+//    метод должен заполниться каждую ячейку массива указанным числом.
+
+    public static void fillArrays(int elementArr, int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = elementArr;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 
 }
