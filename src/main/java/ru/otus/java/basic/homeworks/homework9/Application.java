6bb1119fc1a34e3804a6084a6226d99f8d75b161
Artem <artekof.one@gmail.com>
2024-08-24 21:25:04 +0300

    Выполнено второе задание

--- src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/Application.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        createArrayList(1,15);
+        //createArrayList(1,15);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            arrayList.add(i);
+        }
+        sumElements(arrayList);
 
 
     }
@@ -26,6 +31,18 @@ public class Application {
 
 //    Реализуйте метод, принимающий в качестве аргумента список целых чисел, суммирующий все элементы,
 //    значение которых больше 5, и возвращающий сумму;
+
+    public static int sumElements(ArrayList<Integer> arrayList){
+        int sum = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (i > 5) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
 
 //    Реализуйте метод, принимающий в качестве аргументов целое число и ссылку на список,
 //    метод должен переписать каждую заполненную ячейку списка указанным числом;
